cf6be45e28c672a5221df6bff151c7c338988823
Kavya Agarwal <kavyaag1617@gmail.com>
2026-07-04 17:28:25 +0530

    Allow Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://memora-sigma-six.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
